54942e0ac5d4a703cfadef1be3f148cd3baf0867
Kevin Flores <kevnicflores96@gmail.com>
2024-01-18 20:14:27 -0300

    Infinite scroll added

--- src/hooks/useGetFeed.ts
+++ src/hooks/useGetFeed.ts
@@ -6,13 +6,46 @@ import { useAppDispatch, useAppSelector } from "../redux/hooks";
 export const useGetFeed = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [Load, setLoad] = useState(true)
   const posts = useAppSelector((state) => state.user.feed);
-  const query = useAppSelector((state) => state.user.query);
 
   const dispatch = useAppDispatch();
 
   const service = useHttpRequestService();
 
+  const loadMore = async() =>{
+    if(loading || !Load) return;
+
+    setLoading(true);
+    setError(false);
+
+    try{
+      const lastPostId = posts[posts.length - 1]?.id || '';
+      const newPosts = await service.getPaginatedPosts(5, lastPostId, '');
+
+      if(newPosts.length === 0){
+        setLoad(false);
+      } else{
+        const updatedPosts = [...posts, ...newPosts];
+        dispatch(updateFeed(updatedPosts));
+        dispatch(setLength(updatedPosts.length));
+      }
+    } catch(e){
+      setError(true);
+      console.log(e);
+    }finally{
+      setLoading(false);
+    }
+  };
+
+  useEffect(()=>{
+    loadMore();
+  }, []);
+
+  return {posts, loading, error, loadMore, Load}
+};
+
+/*
   useEffect(() => {
     try {
       setLoading(true);
@@ -30,4 +63,5 @@ export const useGetFeed = () => {
   }, [query]);
 
   return { posts, loading, error };
-};
+  */
+
