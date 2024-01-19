99cbc9ceadeb0057a310e5ff4188328a18ae8d9a
Kevin Flores <kevnicflores96@gmail.com>
2024-01-18 21:10:58 -0300

    Infinite scroll added to ContentFeed

--- src/components/feed/ContentFeed.tsx
+++ src/components/feed/ContentFeed.tsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Feed from "./Feed";
 import { useGetFeed } from "../../hooks/useGetFeed";
 
 const ContentFeed = () => {
-  const { posts, loading } = useGetFeed();
+  const { posts, loading, loadMore, Load } = useGetFeed();
+  const loader = useRef(null);
+  const observer = useRef<IntersectionObserver | null>(null);
 
-  return <Feed posts={posts} loading={loading} />;
+  useEffect(()=>{
+    if(observer.current) observer.current.disconnect();
+
+    observer.current = new IntersectionObserver(
+      entries =>{
+        if(entries[0].isIntersecting && Load){
+          loadMore();
+        }
+      },
+      {
+        root:null,
+        rootMargin: "1px",
+        threshold:1.0
+      }
+    );
+
+    if(loader.current){
+      observer.current.observe(loader.current);
+    }
+
+    return () => {
+      if(observer.current){
+        observer.current.disconnect();
+      }
+    };
+  },[Load, loadMore]);
+
+  // return <Feed posts={posts} loading={loading} />;
+
+  return (
+    <div>
+      <Feed posts={posts} loading={loading} />
+      <div className="loading" ref={loader}></div>
+    </div>
+  )
 };
+
+
 export default ContentFeed;
